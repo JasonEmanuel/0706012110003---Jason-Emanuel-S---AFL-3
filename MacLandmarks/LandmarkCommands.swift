7a7dc5ec83a0278cd4204d495bc6adf27744a675
JasonEmanuel <jemanuel@student.ciputra.ac.id>
2023-04-27 00:04:14 +0700

    Chapter 4 Section 6

--- MacLandmarks/LandmarkCommands.swift
+++ MacLandmarks/LandmarkCommands.swift
@@ -8,8 +8,33 @@
 import SwiftUI
 
 struct LandmarkCommands: Commands {
+    // property wrapper to track the currently selected landmark
+    @FocusedBinding(\.selectedLandmark) var selectedLandmark
+    
     var body: some Commands {
         // include command for toggling the sidebar
         SidebarCommands()
+        
+        // add new command menu
+        CommandMenu("Landmark") {
+            Button("\(selectedLandmark?.isFavorite == true ? "Remove" : "Mark") as Favorite") {
+                selectedLandmark?.isFavorite.toggle()
+            }
+            // set keyboard shortcut
+            .keyboardShortcut("f", modifiers: [.shift, .option])
+            .disabled(selectedLandmark == nil)
+        }
+    }
+}
+
+// extend the Focused Values structure
+private struct SelectedLandmarkKey: FocusedValueKey {
+    typealias Value = Binding<Landmark>
+}
+
+extension FocusedValues {
+    var selectedLandmark: Binding<Landmark>? {
+        get { self[SelectedLandmarkKey.self] }
+        set { self[SelectedLandmarkKey.self] = newValue }
     }
 }
